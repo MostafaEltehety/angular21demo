32d434cbc46a45b893dfbbddaf9fe60e65a56b8c
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-26 00:00:11 +0200

    f

---
+++ src/app/components/side-nav/side-nav.ts
@@ -0,0 +1,30 @@
+import { Component, computed, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-side-nav',
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatListItem,
+    MatIconModule,
+    RouterLink,
+    RouterOutlet,
+    MatToolbarModule,
+    MatNavList
+],
+  templateUrl: './side-nav.html',
+  styleUrl: './side-nav.scss',
+})
+export class SideNav {
+collabse=signal(false);
+sidenavWidth=computed(()=>this.collabse()?'60px':'250px');
+toggle() {
+this.collabse.update(x=>x=!x);
+}
+}
