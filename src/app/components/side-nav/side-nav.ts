97d2d09665daff2c42cbe1b425063f0c44506697
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-26 12:33:47 +0200

    r

--- src/app/components/side-nav/side-nav.ts
+++ src/app/components/side-nav/side-nav.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, ViewEncapsulation } from '@angular/core';
+import { Component, computed, inject, signal, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListItem, MatNavList } from '@angular/material/list';
@@ -6,6 +6,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -27,7 +28,32 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 export class SideNav {
 collabse=signal(false);
 sidenavWidth=computed(()=>this.collabse()?'60px':'250px');
+private translate=inject(TranslateService);
+curentLanguage=signal('ar');
+constructor(){
+  this.translate.setDefaultLang('ar');
+  this.translate.use('ar');
+  this.updateDirection('ar');
+}
 toggle() {
 this.collabse.update(x=>x=!x);
 }
+
+private updateDirection(lang:string){
+const htmlTag=document.documentElement;
+if(lang==='ar'){
+htmlTag.setAttribute('dir','rtl');
+htmlTag.setAttribute('lang','ar');
+}else{
+htmlTag.setAttribute('dir','ltr');
+htmlTag.setAttribute('lang','en');
+}
+}
+SwitchLang(){
+  const newLang=this.curentLanguage()==='ar'?'en':'ar';
+this.curentLanguage.set(newLang);
+this.translate.use(newLang);
+this.updateDirection(newLang);
+}
+
 }
