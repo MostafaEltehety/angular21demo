b3114e59b574a95abb9b1453b56bf4688bde2b35
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-27 01:00:28 +0200

    ح

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-
+import { TranslateModule } from '@ngx-translate/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInput } from "@angular/material/input";
+import { FormControl, FormControlName, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAnchor } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [TranslateModule, MatFormFieldModule, MatInput, ɵInternalFormsSharedModule, ReactiveFormsModule, MatAnchor, MatIcon],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+save() {
+throw new Error('Method not implemented.');
+}
+
+  from=new FormGroup({
+  userName:new FormControl('',Validators.required),
+  password:new FormControl('',Validators.required)
+});
+
 
 }
