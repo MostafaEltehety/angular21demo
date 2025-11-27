f2af543983dfa98b8a19406a08acb5facee400e2
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-28 01:11:28 +0200

    j

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from "@angular/router";
-import { ThemeToggleComponent } from '../../components/theme-toggle/theme-toggle';
-import { MatFormField, MatLabel } from "@angular/material/form-field";
-import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-home',
-  imports: [MatFormField, MatLabel,MatIconModule,MatInputModule,MatButtonModule],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
+
 }
-RouterOutlet
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,25 +1,56 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInput } from "@angular/material/input";
-import { FormControl, FormControlName, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAnchor } from "@angular/material/button";
-import { MatIcon } from "@angular/material/icon";
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  Validators,
+  ɵInternalFormsSharedModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatAnchor } from '@angular/material/button';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-login',
-  imports: [TranslateModule, MatFormFieldModule, MatInput, ɵInternalFormsSharedModule, ReactiveFormsModule, MatAnchor ],
+  imports: [
+    TranslateModule,
+    MatFormFieldModule,
+    MatInput,
+    ɵInternalFormsSharedModule,
+    ReactiveFormsModule,
+    MatAnchor,
+    NgClass,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-save() {
-throw new Error('Method not implemented.');
-}
+  form = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  from=new FormGroup({
-  userName:new FormControl('',Validators.required),
-  password:new FormControl('',Validators.required)
-});
+  private _snackBar = inject(MatSnackBar);
+  private _translate = inject(TranslateService);
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
-
+  openSnackBar() {
+    this._snackBar.open(
+      this._translate.instant('LOGIN_SUCCESS'),
+      this._translate.instant('CLOSE'),
+      {
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        duration: 3000,
+      }
+    );
+  }
 }
