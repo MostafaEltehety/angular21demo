357623e5f38aff0edc542f710c743ea43204e919
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-30 15:45:16 +0200

    e

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,15 +12,31 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   {
     path: '',
-    component:SideNav,
+    component: SideNav,
     children: [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: Home },
       { path: 'departments', component: Departments },
-      {path:'employees',component:Employee},
-{path:'signal',component:Signal}
+      { path: 'employees', component: Employee },
+      { path: 'signal', component: Signal }
     ],
   },
 
+
+  {
+    path: 'blank',
+    loadComponent: () => import('./pages/e-commerce/layouts/blank-layout/blank-layout').then((m) => m.BlankLayout),
+    children: [
+
+      {path: 'home',loadComponent: () => import('./pages/e-commerce/components/home/home').then((m) => m.Home)},
+      { path: 'product', loadComponent: () => import('./pages/e-commerce/components/product/product').then(m => m.Product) },
+      { path: 'cart', loadComponent: () => import('./pages/e-commerce/components/cart/cart').then(m => m.Cart) }
+
+    ]
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./pages/e-commerce/layouts/auth-layout/auth-layout').then((m) => m.AuthLayout)
+  },
   { path: '**', redirectTo: '/login' },
 ];
