6db198e04462f355ff0b39ba4a548019dcf36887
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-12-24 23:29:47 +0200

    b

--- src/app/pages/departments/departments.ts
+++ src/app/pages/departments/departments.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { department } from '../../models/demaprtment';
 import { MatAnchor } from '@angular/material/button';
 import { TranslateModule } from '@ngx-translate/core';
@@ -8,10 +8,25 @@ import { DialogModule, DialogRef } from '@angular/cdk/dialog';
 import { DepartmentForm } from './department-form/department-form';
 import { from } from 'rxjs';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortHeader } from "@angular/material/sort";
 
 @Component({
   selector: 'app-departments',
-  imports: [MatAnchor, TranslateModule, MatIconModule, MatDialogModule, MatTableModule],
+  imports: [
+    MatAnchor,
+    TranslateModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSort,
+    MatSortHeader
+],
   templateUrl: './departments.html',
   styleUrl: './departments.scss',
   standalone: true,
@@ -19,17 +34,33 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 export class Departments {
   allDepartments: Array<department> = [
     { id: 1, name: 'Web Dev', description: 'Web Development' },
-    { id: 2, name: 'QA', description: 'Quality Assurance' },
-    { id: 3, name: 'BA', description: 'Business Analysis' },
-    { id: 4, name: 'Mob Dev', description: 'Mobile Development' },
+    { id: 2, name: 'BA', description: 'Business Analysis' },
+    { id: 3, name: 'Mob Dev', description: 'Mobile Development' },
+    { id: 4, name: 'QA', description: 'Quality Assurance' },
+    { id: 5, name: 'Desk Dev', description: 'Desktop Development' },
+    { id: 6, name: 'PM', description: 'Project Management' }
   ];
 
   displayColumns: string[] = ['id', 'name', 'description'];
+  // @ViewChild(MatPaginator)
+  // paginator!: MatPaginator;
+  @ViewChild(MatPaginator) deptPaginator!: MatPaginator;
+
+  // @ViewChild(MatSort)
+  // depSort!: MatSort;
+  @ViewChild(MatSort) deptSort!: MatSort;
 
   departmentsDataSource = new MatTableDataSource<any>();
 
   ngAfterViewInit() {
     this.departmentsDataSource.data = this.allDepartments;
+    this.departmentsDataSource.paginator = this.deptPaginator;
+    this.departmentsDataSource.sort = this.deptSort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterInputValue = (event.target as HTMLInputElement).value;
+    this.departmentsDataSource.filter = filterInputValue;
   }
 
   dialog = inject(MatDialog);
