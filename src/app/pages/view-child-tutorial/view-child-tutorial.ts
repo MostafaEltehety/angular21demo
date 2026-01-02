91a4812da814bd5b2ea5c30bae60793d0fe507de
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2026-01-02 14:59:33 +0200

    s

--- src/app/pages/view-child-tutorial/view-child-tutorial.ts
+++ src/app/pages/view-child-tutorial/view-child-tutorial.ts
@@ -1,12 +1,14 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { MatFormField } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
 import { MatAnchor } from "@angular/material/button";
-import { Greetings } from "../../greetings/greetings";
+import { Greetings } from '../../greetings/greetings';
+
+
 
 @Component({
   selector: 'app-view-child-tutorial',
-  imports: [MatFormField, MatInput, MatAnchor, Greetings],
+  imports: [MatFormField, MatInput, MatAnchor,Greetings],
   templateUrl: './view-child-tutorial.html',
   styleUrl: './view-child-tutorial.scss',
 })
@@ -16,7 +18,8 @@ export class ViewChildTutorial  {
   @ViewChild('nameTempeleteRefVariable') inputRef!:ElementRef;
   @ViewChild('emailInput') inputEmailRef!:ElementRef;
   @ViewChild('box') boxRef!:ElementRef;
-  @ViewChild(Greetings) say!:Greetings;
+  @ViewChild(Greetings,{static:false}) say!:Greetings;
+ @ViewChildren(Greetings) allGreetings!:Greetings[];
 foucsInput() {
 this.inputRef.nativeElement.foucs;
 this.inputRef.nativeElement.value="Mostafa Ahmed";
@@ -32,6 +35,9 @@ this.boxRef.nativeElement.style.border='10px solid red';
 
 greeting() {
 this.say.greetUser();
+}
+callAllGreetings(){
+  this.allGreetings.forEach(g=>g.greetUser());
 }
 
 }
