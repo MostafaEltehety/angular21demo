ce9414e2ae01992f44dfe61bd812e517865e9311
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2026-01-02 18:20:52 +0200

    p

--- src/app/lessons/ng-template/ng-template.ts
+++ src/app/lessons/ng-template/ng-template.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
  import { MatSlideToggle } from "@angular/material/slide-toggle";
- import { CommonModule, NgFor, NgIf } from '@angular/common';
+ import { CommonModule } from '@angular/common';
 import { MatAnchor } from "@angular/material/button";
 import { CdkNoDataRow } from "@angular/cdk/table";
 
 @Component({
   selector: 'app-ng-template',
-  imports: [FormsModule, MatSlideToggle, CommonModule, MatAnchor, CdkNoDataRow],
+  imports: [FormsModule, MatSlideToggle, CommonModule, MatAnchor],
   templateUrl: './ng-template.html',
   styleUrl: './ng-template.scss',
   standalone: true,
